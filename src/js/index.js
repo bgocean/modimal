cc3f1a0b02b20b2424aa355a65842769d9d39077
bgocean <bigocean700@gmail.com>
2025-01-03 00:25:29 +0200

    add collection mobile

--- src/js/index.js
+++ src/js/index.js
@@ -12,9 +12,16 @@ import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
-  spaceBetween: 24,
-  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 16,
+  // loop: true,
+  breakpoints: {
+    780: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
