88e0b48cd85a1cdfc46e2c97c83aa7a798416f68
bgocean <bigocean700@gmail.com>
2025-01-06 03:12:31 +0200

    add burger menu buttons

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,5 +1,20 @@
 function accordion() {
-  console.log("accordion is here");
+  const btns = document.querySelectorAll(".mobile-links__parent-btn");
+
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const content = btn.nextElementSibling;
+      // console.log(content);
+
+      const isOpen = btn.classList.toggle("active");
+
+      if (isOpen) {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        content.style.maxHeight = "0px";
+      }
+    });
+  });
 }
 
 export default accordion;
